5104bec3cbf3f4772a383a72b6c21b1b5d7e37fd
Kazuki Chigita <kazuki.chigichan@gmail.com>
2019-04-01 21:12:06 +0900

    implement background splash

--- app/src/main/java/com/example/sampledisplayoverlay/MainActivity.kt
+++ app/src/main/java/com/example/sampledisplayoverlay/MainActivity.kt
@@ -1,30 +1,37 @@
 package com.example.sampledisplayoverlay
 
-import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val view: View by lazy {
+        findViewById<View>(com.example.sampledisplayoverlay.R.id.splash)
+    }
+    val button1: Button by lazy {
+        findViewById<Button>(R.id.button1)
+    }
+    val button2: Button by lazy {
+        findViewById<Button>(R.id.button2)
+    }
+    val button3: Button by lazy {
+        findViewById<Button>(R.id.button3)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-        val view: View = findViewById(R.id.splash)
-        val button1: Button = findViewById(R.id.button1)
-        val button2: Button = findViewById(R.id.button2)
-        val button3: Button = findViewById(R.id.button3)
-
         val anim = AlphaAnimation(1f, 0f)
         anim.duration = 2000
-        anim.setAnimationListener(object : Animation.AnimationListener{
+        anim.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(p0: Animation?) {
                 // NOP
             }
@@ -43,7 +50,7 @@ class MainActivity : AppCompatActivity() {
 
         view.startAnimation(anim)
 
-        button1.setOnClickListener{
+        button1.setOnClickListener {
             startActivity(
                 Intent(this, DefaultActivity::class.java)
             )
@@ -60,6 +67,22 @@ class MainActivity : AppCompatActivity() {
                 Intent(this, SplashOverlayActivity::class.java)
             )
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        button1.visibility = View.GONE
+        button2.visibility = View.GONE
+        button3.visibility = View.GONE
+        view.visibility = View.VISIBLE
+    }
+
+    override fun onResume() {
+        super.onResume()
+        button1.visibility = View.VISIBLE
+        button2.visibility = View.VISIBLE
+        button3.visibility = View.VISIBLE
+        view.visibility = View.GONE
     }
 
     private fun visibleAllButton(buttonList: List<Button>) {
